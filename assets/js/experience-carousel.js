8030da20f3c91b42165d2ad936166ff79e6f339e
Victor Mateevitsi <mvictoras@gmail.com>
2020-09-04 11:30:31 -0500

    Switched to slick-carousel

--- assets/js/experience-carousel.js
+++ assets/js/experience-carousel.js
@@ -1,19 +1,22 @@
-$('.carousel-item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-
-  for (var i=0;i<4;i++) {
-    next=next.next();
-    if (!next.length) {
-      next=$(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-  }
-});
-
-$('#carousel-experience').carousel({
-  interval: 5000,
+$('.carousel-experience').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
