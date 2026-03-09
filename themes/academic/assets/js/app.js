f663b4fa756e116b229266072eacaf46b6fee6d5
Victor Mateevitsi <mvictoras@gmail.com>
2026-03-09 15:24:18 -0500

    Upgraded PhotoSwipe 4 to 5 with Lightbox API
    Replaced v4 jQuery init + boilerplate DOM with v5 Lightbox component.
    Removed PhotoSwipe module mounts and vendor script entries.
    Added PurgeCSS greedy safelist for dynamically-created pswp classes.

--- themes/academic/assets/js/app.js
+++ themes/academic/assets/js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-/* global anime, PhotoSwipe, PhotoSwipeUI_Default */
-
 import { listen } from 'quicklink/dist/quicklink.mjs';
 import jquery from 'jquery';
 import 'lazysizes';
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import PhotoSwipe from 'photoswipe';
 //import anime from 'animejs/lib/anime.es.js';
 
 window.jQuery = jquery;
@@ -13,41 +13,14 @@ listen();
 
 (function($) {
 
-  // Init empty gallery array
-  var container = [];
+  if (document.querySelector('#gallery')) {
+    const lightbox = new PhotoSwipeLightbox({
+      gallery: '#gallery',
+      children: 'a',
+      pswpModule: PhotoSwipe,
+    });
 
-  // Loop over gallery items and push it to the array
-  $('#gallery').find('figure').each(function() {
-    var $link = $(this).find('a'),
-      item = {
-        src: $link.attr('href'),
-        w: $link.data('width'),
-        h: $link.data('height'),
-        title: $link.data('caption'),
-      };
-    container.push(item);
-  });
-
-  // Define click event on gallery item
-  $('#gallery').find('a').click(function(event) {
-
-    // Prevent location change
-    event.preventDefault();
-
-    // Define object and gallery options
-    var $pswp = $('.pswp')[0],
-      options = {
-        index: $(this).parent('figure').index(),
-    bgOpacity: 0.8,
-    captionEl: false,
-    tapToClose: true,
-    shareEl: false,
-    fullscreenEl: false,
-      };
-
-    // Initialize PhotoSwipe
-    var gallery = new PhotoSwipe($pswp, PhotoSwipeUI_Default, container, options);
-    gallery.init();
-  });
+    lightbox.init();
+  }
 
 }(jQuery));
